e3b99829c455029668038b1c745e4be72cf1d391
tilko <tilen.plahuta1@gmail.com>
2023-09-21 11:31:40 +0200

    python template dodan

--- python_template/main.py
+++ python_template/main.py
@@ -1,6 +1,20 @@
+#! /usr/bin/python2
+import time
+
+
+i = 3
+global n 
+n = 2
+
 
 def main():
+    global n
+    print(str(n))
+    print("To je moje sporocilo: %f" % n)
+    i = 2
     print("Hello World!")
+    
+
 
 if __name__ == "__main__":
     main()
